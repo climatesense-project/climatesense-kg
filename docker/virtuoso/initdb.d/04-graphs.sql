aa0053b0a2feacd013932df9721f6c61d5d5a3eb
Thibault Ehrhart <thibault.ehrhart@gmail.com>
2026-08-06 10:38:48 +0200

    chore: rename 04-ontology.sql to 04-graphs.sql
    The script loads vocabularies and the catalog graph, not an ontology.

--- docker/virtuoso/initdb.d/04-graphs.sql
+++ docker/virtuoso/initdb.d/04-graphs.sql
@@ -4,6 +4,7 @@
 
 delete from DB.DBA.LOAD_LIST;
 ld_dir('/database/data/vocabularies', '*.ttl', 'http://data.climatesense-project.eu/graph/vocabularies');
+ld_dir('/database/data', 'graphs.ttl', 'http://data.climatesense-project.eu/graph/catalog');
 rdf_loader_run();
 checkpoint;
 
